9b9750bbb68250538136f1641f2fc10aa120ab31
Jesse Cranney <jesse.cranney@anu.edu.au>
2025-09-13 11:06:07 +1000

    basic concepts done

--- surge-arduinoif/README.md
+++ surge-arduinoif/README.md
@@ -1,4 +1,11 @@
-## Arduino Interface Service `surge-arduinoif`
+# Arduino Interface Service `surge-arduinoif`
 
 The purpose of this service is to receive commands from external processes, and then translate/communicate them with the arduino uno over serial port.
 
+## Requirements
+On top of the python requirements, there is also the command line tool: `mount_clt`, which is available at [jcranney/mount-clt](https://github.com/jcranney/mount-clt).
+
+## Usage
+```bash
+flask --app=main.py run --host=0.0.0.0 --port=5000
+```
--- surge-arduinoif/main.py
+++ surge-arduinoif/main.py
@@ -3,14 +3,38 @@ from pydantic import BaseModel
 from flask_openapi3 import Info
 from flask_openapi3 import OpenAPI
 import subprocess
+from enum import Enum
 
 info = Info(title="Arduino Interface API", version="1.0.0")
 app = OpenAPI(__name__, info=info)
 
 
+class Direction(str, Enum):
+    forward = "forward"
+    backward = "backward"
+
+
+class Motor(str, Enum):
+    a = "a"
+    b = "b"
+
+
+class Device(str, Enum):
+    devnull = "/dev/null"
+
+
+class Buffer(str, Enum):
+    period = "period"
+    hightime = "hightime"
+
+
 class ArduinoCommand(BaseModel):
-    motor: str
+    device: Device | None = None
+    motor: Motor
     enabled: bool
+    direction: Direction
+    buffer: Buffer
+    value: int
 
 
 @app.post("/command", summary="send a motor command")
@@ -18,16 +42,33 @@ def post_command(query: ArduinoCommand):
     """
     post a command to the arduino over serial
     """
-    result = subprocess.run(["mount_clt", "--help"])
+    command = ["mount_clt"]
+    if query.device is not None:
+        command += [f"--device={query.device.value}"]
+    command += [
+        "full",
+        query.motor.value,
+        "enabled" if query.enabled else "disabled",
+        query.direction.value,
+        query.buffer.value,
+        f"{query.value}",
+    ]
+    print(command)
+    result = subprocess.run(command, capture_output=True)
     print(result)
-    return {
-        "code": 0,
-        "message": "ok",
-        "data": [
-            {"bid": 1, "age": query.motor, "author": query.enabled},
-            {"bid": 2, "age": query.motor, "author": query.enabled}
-        ]
-    }
+    if result.returncode == 0:
+        # success
+        return {
+            "message": "ok",
+            "data": [
+                "message sent successfully"
+            ]
+        }
+    else:
+        # something went wrong
+        return {
+            "message": result.stderr.decode()
+        }, 500
 
 
 if __name__ == "__main__":
